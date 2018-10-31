969c411ec0769ca2d58ad1bf2c4092457f0d94e6
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-10-31 16:48:35 -0500

    clean up and comment edit festival component

--- src/components/EditFestival/EditFestival.js
+++ src/components/EditFestival/EditFestival.js
@@ -1,55 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+// material-ui imports
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
+import TextField from '@material-ui/core/TextField';
+// connect to redux
 import { connect } from 'react-redux';
 
-class ResponsiveDialog extends React.Component {
-  
-    state = {
+class EditFestival extends Component {
+
+  // define state and set properties equal the current festival information
+  state = {
+    // dialog not open
     open: false,
     name: this.props.festToRespond.name,
     date: this.props.festToRespond.date,
     address: this.props.festToRespond.address,
     image: this.props.festToRespond.image,
+    // get clicked festival from redux state
     id: this.props.festToRespond.id,
   };
 
+  // handle changes in inputs, curried function
   handleChangeFor = property => event => {
     this.setState({
       ...this.state,
       [property]: event.target.value,
     });
-  }
+  }; // end handleChangeFor
 
+  // open dialog 
   handleClickOpen = () => {
-    this.props.dispatch({type: 'FESTIVAL_RESPONSE', payload: this.props.festToRespond});
+    // this.props.dispatch({type: 'FESTIVAL_RESPONSE', payload: this.props.festToRespond});
     this.setState({ open: true });
-  };
+  }; // end handleClickOpen
 
+  // closes dialog
   handleClose = () => {
     this.setState({ open: false });
-  };
+  }; // end handleClose
 
-
+  // handles click of update button
   updateState = () => {
+    // dispatch action to update festival information in database
     this.props.dispatch({ type: 'UPDATE_FESTIVAL', payload: this.state });
-    this.props.dispatch({type: 'FESTIVAL_RESPONSE', payload: this.state});
+    // dispatch action to update festival in redux state
+    this.props.dispatch({ type: 'FESTIVAL_RESPONSE', payload: this.state });
     this.handleClose();
-  }
+  } // end updateState
 
-  componentDidMount() {
-    
-  }
 
   render() {
 
-    const {fullScreen} = this.props;
+    const { fullScreen } = this.props;
 
     return (
       <div>
@@ -61,26 +68,46 @@ class ResponsiveDialog extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="responsive-dialog-title"
         >
+        {/* dialog content */}
           <DialogTitle id="responsive-dialog-title">{"Edit Festival Information"}</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              <label> Festival Name 
-                <input type="text" value={this.state.name} onChange={this.handleChangeFor('name')} />
-              </label>
-              <br />
-              <label> Date 
-                <input type="date" value={this.state.date} onChange={this.handleChangeFor('date')} />
-              </label>
-              <br />
-              <label> Address 
-                <input type="text" value={this.state.address} onChange={this.handleChangeFor('address')} />
-              </label>
-              <br />
-              <label> Image Url 
-                <input type="text" value={this.state.image} onChange={this.handleChangeFor('image')} />
-              </label>
-              <br />
-            </DialogContentText>
+            <br />
+            <TextField
+              type="text"
+              label="Festival Name"
+              variant="outlined"
+              value={this.state.name}
+              onChange={this.handleChangeFor('name')}
+            />
+            <br />
+            <br />
+            <TextField
+                type="date"
+                label="Date"
+                variant="outlined"
+                value={this.state.date}
+                onChange={this.handleChangeFor('date')}
+              />
+            <br />
+            <br />
+            <TextField
+              type="text"
+              label="Address"
+              variant="outlined"
+              value={this.state.address}
+              onChange={this.handleChangeFor('address')}
+            />
+            <br />
+            <br />
+            <TextField
+              type="text"
+              label="Image Url"
+              variant="outlined"
+              value={this.state.image}
+              onChange={this.handleChangeFor('image')}
+            />
+            <br />
+            <br />
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="secondary">
@@ -96,16 +123,15 @@ class ResponsiveDialog extends React.Component {
   }
 }
 
-ResponsiveDialog.propTypes = {
+EditFestival.propTypes = {
   fullScreen: PropTypes.bool.isRequired,
 };
 
-
+// connect to redux state to get info of clicked festival
 const mapStateToProps = state => {
   return {
     festToRespond: state.festToRespond,
   };
 }
 
-
-export default connect(mapStateToProps)(withMobileDialog()(ResponsiveDialog));
+export default connect(mapStateToProps)(withMobileDialog()(EditFestival));
