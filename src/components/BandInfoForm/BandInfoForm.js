c15ad802facc05ad93c30385af8b8657f8f3dd16
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-10-31 15:07:32 -0500

    clean up and comment band info form component

--- src/components/BandInfoForm/BandInfoForm.js
+++ src/components/BandInfoForm/BandInfoForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+// connect component to redux
 import { connect } from 'react-redux';
+// my component, displays after initial upload of tour information
 import EditBandInfo from '../EditBandInfo/EditBandInfo';
+// material-ui imports
 import UppyModalWithButton from '../UppyModalWithButton/UppyModalWithButton';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
@@ -10,13 +13,8 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-
+// jss styles
 const styles = {
-    h2: {
-        textAlign: 'center',
-        color: 'DarkSlateGray',
-        margin: 40,
-    },
     card: {
         margin: 'auto',
         maxWidth: 700,
@@ -25,27 +23,32 @@ const styles = {
 }
 
 class BandInfoForm extends Component {
-
+    // define state and create properties to be controlled by inputs on DOM (except for person_id)
     state = {
         name: '',
         tech_rider: '',
         band_rider: '',
         stage_plot: '',
         input_list: '',
+        // gets person_id from redux state
         person_id: this.props.user.id,
     }
 
+    // handle changes in inputs on DOM, curried function
     handleChangeFor = propertyName => event => {
         this.setState({
+            // only change property name passed in, set to what user types into input
             ...this.state,
             [propertyName]: event.target.value
         });
-    };
+    }; // end handleChangeFor
 
+    // handle form submission
     handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
+        event.preventDefault(); // stop page refresh
+        // dispatch action to add band info, send state
         this.props.dispatch({ type: 'ADD_BAND_INFO', payload: this.state });
+        // reset inputs
         this.setState({
             state: {
                 name: '',
@@ -56,18 +59,19 @@ class BandInfoForm extends Component {
             }
         });
 
-    };
+    }; // end handleSubmit
 
+    // handle uppy file uploads, curried function
     handleUploadInputFor = (property) => {
         return (uploadURL) => {
-            console.log(uploadURL);
             this.setState({
                 ...this.state,
                 [property]: uploadURL,
             })
         }
-    }
+    }; // end handleUploadInputFor
 
+    // get band info on page load, specific to user, gets info by user's id
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_BAND_INFO', payload: this.props.user.id });
     }
@@ -80,16 +84,17 @@ class BandInfoForm extends Component {
 
         return (
             <div>
+                {/* if user has previously uploaded band info, show info and edit info button */}
                 {this.props.band_info.id ?
                     <>
-                        <h2 className={classes.h2}>Your information is stored! Click the Update Tour Information button to edit.</h2>
+                        <h2 className="tagline" style={{margin: 40}}>Your information is stored! Click the Update Tour Information button to edit.</h2>
                         <EditBandInfo />
                         <br />
                         <br />
                         <div className="cardDiv">
                             <Card className={classes.card}>
                                 <CardContent>
-                                    <Typography variant="h3">
+                                    <Typography variant="h4">
                                         Current Tour Information
                                     </Typography>
                                     <hr />
@@ -115,11 +120,12 @@ class BandInfoForm extends Component {
                             <br />
                         </div>
                     </>
+                    // if the user has not previously uploaded tour information, show the initial upload form
                     :
                     <>
                         <form onSubmit={this.handleSubmit}>
                             <div className="inputDiv">
-                            <h2 className={classes.h2}>Please upload current band information.</h2>
+                            <h2 className="tagline" style={{margin: 40}}>Please upload current band information.</h2>
                                 <br />
                                 <TextField
                                     type="text"
@@ -137,6 +143,7 @@ class BandInfoForm extends Component {
                                     value={this.state.tech_rider}
                                     onChange={this.handleChangeFor('tech_rider')}
                                 />
+                                {/* bring in uppy component to handle file uploads */}
                                 <UppyModalWithButton handleUploadInput={this.handleUploadInputFor('tech_rider')} />
                                 <br />
                                 <br />
@@ -195,7 +202,7 @@ class BandInfoForm extends Component {
 BandInfoForm.propTypes = {
     classes: PropTypes.object.isRequired,
 };
-
+// necessary to use information from the redux state
 const mapStateToProps = state => {
     return {
         user: state.user,
