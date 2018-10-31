625a9aea7999e2a6f88c823904e2b15225f81f36
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-10-31 09:38:16 -0500

    add text to about page, take out info page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+// this is a very simple component with some text
 
 const AboutPage = () => (
   <div>
     <div>
+      <form style={{width: 500, fontSize: 24}}>
       <p>
-        This about page is for anyone to read!
+      Miscommunication and lost emails are common and a source of great frustration in the world of professional live concert production. 
+      Festival Form  was developed to help streamline communication between music festival organizers and band tour managers to create well-organized festivals. 
+      Band tour managers are able to upload their current information and RSVP to music festivals. 
+      Music festival organizers can create festivals, see who has responded to each festival, and download each respondents information.
       </p>
+      </form>
     </div>
   </div>
 );
