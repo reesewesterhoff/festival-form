70f97c57ec9b1cd5e390f7cb997fd297fd8989b9
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-10-31 17:05:14 -0500

    clean up and comment festival detail component

--- src/components/FestivalDetail/FestivalDetail.js
+++ src/components/FestivalDetail/FestivalDetail.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+// connect to redux
 import { connect } from 'react-redux';
+// import moment.js to handle times and dates
 import moment from 'moment';
+// material-ui imports
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -10,7 +13,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DoneAllIcon from '@material-ui/icons/DoneAll';
 
-
+// jss styles
 const styles = {
     card: {
         marginTop: 30,
@@ -31,34 +34,36 @@ const styles = {
         color: 'CornflowerBlue',
         margin: 10,
     },
-    h1: {
-        textAlign: 'center',
-    },
 };
 
 class FestivalDetail extends Component {
-
+    // define state
     state = {
         festivals: [],
         response: {
+            // set festival response to the users current band information
             name: this.props.band_info.name,
             tech_rider: this.props.band_info.tech_rider,
             band_rider: this.props.band_info.band_rider,
             stage_plot: this.props.band_info.stage_plot,
             input_list: this.props.band_info.input_list,
+            // user enters these on the page
             arrival_time: '',
             requests: '',
             notes: '',
+            // send festival id and band info id so individual 
+            // bands can be called back to specific festivals
             festival_id: this.props.festToRespond.id,
             band_info_id: this.props.band_info.id,
         }
     }
 
+    // scroll the window back to the top of the page
     componentDidMount() {
         window.scrollTo(0, 0);
     }
 
-
+    // handle changes in inputs, curried function
     handleChangeFor = propertyName => event => {
         this.setState({
             response: {
@@ -67,11 +72,14 @@ class FestivalDetail extends Component {
             }
 
         });
-    };
+    }; // end handleChangeFor
 
+    // handle form submission
     handleSubmit = (event) => {
-        event.preventDefault();
+        event.preventDefault(); // prevent refresh
+        // dispatch action to add a response to the specified festival, send along users band info
         this.props.dispatch({ type: 'ADD_RESPONSE', payload: this.state.response });
+        // reset inputs
         this.setState({
             response: {
                 arrival_time: '',
@@ -79,7 +87,7 @@ class FestivalDetail extends Component {
                 notes: ''
             }
         });
-    };
+    }; // end handleSubmit
 
 
     render() {
@@ -90,6 +98,7 @@ class FestivalDetail extends Component {
         return (
             <div>
                 <div>
+                    {/* instructions to user */}
                     <div className="tagline">
                         <h1>Festival RSVP</h1>
                         <p>Please verify that you are responding to the correct festival than enter your information.</p>
@@ -145,6 +154,7 @@ class FestivalDetail extends Component {
                 <div>
                     {fest.id ?
                         <div>
+                            {/* card displays which festival they are responding to */}
                             <Card className={classes.card}>
                                 <CardContent>
                                     <Typography variant="h3" gutterBottom>
@@ -152,6 +162,7 @@ class FestivalDetail extends Component {
                                         <br />
                                     </Typography>
                                     <Typography className={classes.countDown} color="textSecondary" variant="h6">
+                                        {/* countdown to festival, format with moment.js */}
                                         Happening {moment(fest.date, "YYYYMMDD").fromNow()}
                                     </Typography>
                                     <Typography>
@@ -160,6 +171,7 @@ class FestivalDetail extends Component {
                                     </Typography>
                                     <br />
                                     <Typography className={classes.pos} color="textSecondary" variant="h5">
+                                        {/* format date with moment.js */}
                                         {moment(fest.date).format('M-DD-YYYY')}
                                         <br />
                                         {fest.address}
@@ -182,6 +194,7 @@ FestivalDetail.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
+// connect to redux state to access information
 const mapStateToProps = state => {
     return {
         state,
