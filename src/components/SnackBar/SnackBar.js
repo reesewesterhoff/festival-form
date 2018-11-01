ecae251564a5e300dff8fb5399bc8627361ed90b
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-11-01 13:47:23 -0500

    add close icon to snack bar

--- src/components/SnackBar/SnackBar.js
+++ src/components/SnackBar/SnackBar.js
@@ -1,17 +1,22 @@
 import React from 'react';
+// material-ui imports
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 
 class SnackBar extends React.Component {
+  //define state
   state = {
     open: false,
     vertical: 'top',
     horizontal: 'center',
   };
 
+  // handles opening of snackbar
   handleClick = state => () => {
     this.setState({ open: true, ...state });
-  };
+  }; // end handleClick
 
   handleClose = () => {
     this.setState({ open: false });
@@ -40,6 +45,16 @@ class SnackBar extends React.Component {
             'aria-describedby': 'message-id',
           }}
           message={<span id="message-id">{this.props.message}</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="secondary"
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
         />
       </div>
     );
