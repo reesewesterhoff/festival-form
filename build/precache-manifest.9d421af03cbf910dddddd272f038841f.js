01452be75e2b45b86a43367aea4c52dc60ef2f5f
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-11-06 11:35:50 -0600

    create build for deployment to heroku

---
+++ build/precache-manifest.9d421af03cbf910dddddd272f038841f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e57ef96b4e7e4e94e4e",
+    "url": "/static/js/main.0e57ef96.chunk.js"
+  },
+  {
+    "revision": "a99feb9296e4c86125ae",
+    "url": "/static/js/1.a99feb92.chunk.js"
+  },
+  {
+    "revision": "0e57ef96b4e7e4e94e4e",
+    "url": "/static/css/main.62d8c9ef.chunk.css"
+  },
+  {
+    "revision": "af2256554770f585281f6224375bd163",
+    "url": "/index.html"
+  }
+];
